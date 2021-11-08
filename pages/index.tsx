4742ae273b4f18e0df5cea3424fb5fb0794bab8a
maull <maull@localhost.localdomain>
2021-11-08 23:28:36 +0700

    chakra ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
+import { Flex, Heading, Input, Button, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  const { toggleColorMode } = useColorMode()
+  const formBackground = useColorModeValue("gray.100", "gray.700")
+  return (
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex direction="column" background={formBackground} p={12} rounded={6}>
+        <Heading mb={6}>Login</Heading>
+        <Input placeholder="username / email" variant="filled" mb={3} type="email"></Input>
+        <Input placeholder="******" variant="filled" mb={6} type="password"></Input>
+        <Button mb={6} colorScheme="teal">Login</Button>
+        <Button onClick={toggleColorMode}>Mode</Button>
+      </Flex>
+    </Flex>
+  )
+}
 
 export default IndexPage
